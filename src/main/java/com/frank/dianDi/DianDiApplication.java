719a896ef767eb468f3c53da9ad5a65e0a21057d
frankLiang <frankliang442@gmail.com>
2023-11-13 22:22:32 +0800

    init project

---
+++ src/main/java/com/frank/dianDi/DianDiApplication.java
@@ -0,0 +1,15 @@
+package com.frank.dianDi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author frank
+ * @date 2023/11/13
+ **/
+@SpringBootApplication
+public class DianDiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DianDiApplication.class);
+    }
+}

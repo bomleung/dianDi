18961edde5af23572155036d87d7048614276bb8
frankLiang <frankliang442@gmail.com>
2023-12-02 17:12:17 +0800

    solve cors problem

---
+++ src/main/java/com/frank/diandi/config/web/CorsFilterConfiguration.java
@@ -0,0 +1,30 @@
+package com.frank.diandi.config.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author frank
+ * @date 2023/12/02
+ **/
+@Configuration
+public class CorsFilterConfiguration {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
+
+
+    }
+}
